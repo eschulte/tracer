b632fbff2f3c26ef8d3462488cf8ce4184047a74
Eric Schulte <eric.schulte@gmx.com>
2011-12-13 21:06:18 -0700

    more general argument passing

--- hello.c
+++ hello.c
@@ -1,1 +1,6 @@
-main(){ puts("hello world"); }
+main(){
+  int i=0;
+  int j=0;
+  for(i=0;i<100;i++) j=i;
+  puts("hello world");
+}
--- ptracer.c
+++ ptracer.c
@@ -2,43 +2,38 @@
 #include <signal.h>
 #include <syscall.h>
 #include <sys/ptrace.h>
+#include <sys/user.h>
+// adapted from
+// http://www.comp.glam.ac.uk/pages/staff/hggross/IntelligentTimingAnalysis_HTML/node20.html
 
-void main (void) {
-
-  long long counter = 1; // machine instruction counter
+void main(int argc, char *argv[]) {
   int  wait_val;         // child's return value
   int  pid;              // child's process id
 
+  struct user_regs_struct regs;
+
   switch (pid=fork()) {
   case -1: perror("fork"); break;
-  case 0:                     // child process starts
+  case 0: // child
     ptrace(0,0,0,0);
-    // must be called in order to allow the
-    // control over the child process
-    execl("./hello","hello",NULL);
-    // executes the testprogram and causes
-    // the child to stop and send a signal 
-    // to the parent, the parent can now
-    // switch to PTRACE_SINGLESTEP
+    // must be called in order to allow the control over the child process
+    execvp(argv[1], &argv[1]);
+    // executes the testprogram and causes the child to stop and send
+    // a signal to the parent, the parent can now switch to PTRACE_SINGLESTEP
     break;
     // child process ends
-  default:                    // parent process starts
+  default: // parent
     wait(&wait_val);
     // parent waits for child to stop (execl)
-    if (ptrace(PTRACE_SINGLESTEP,pid,0,0) != 0)
-      perror("ptrace");
+    if (ptrace(PTRACE_SINGLESTEP,pid,0,0) != 0)          perror("ptrace");
     // switch to singlestep tracing and release child
     wait(&wait_val);
     // wait for child to stop at next instruction
     while (wait_val == 1407) {
-      counter++;
-      if (ptrace(PTRACE_SINGLESTEP,pid,0,0) != 0) 
-        perror("ptrace");
+      if (ptrace(PTRACE_SINGLESTEP,pid,0,0) != 0)        perror("ptrace");
+      if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) != 0) perror("ptrace");
+      printf("0x%x\n", regs.rip);
       wait(&wait_val);
     }
-    // continue to stop, wait and release until
-    // the child is finished; wait_val != 1407
-    // Low=0177L and High=05 (SIGTRAP)
   }
-  printf("Counter: %d\n", counter);
 }
