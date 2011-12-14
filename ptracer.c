068fe603d7c0ad2ea4f9dec3fbf69507ce0ee726
Eric Schulte <eric.schulte@gmx.com>
2011-12-13 21:13:45 -0700

    redirecting the child processes STDOUT and STDERR to /dev/null

--- ptracer.c
+++ ptracer.c
@@ -1,20 +1,26 @@
 #include <stdlib.h>
-#include <signal.h>
+#include <stdio.h>
 #include <syscall.h>
 #include <sys/ptrace.h>
 #include <sys/user.h>
 // adapted from
 // http://www.comp.glam.ac.uk/pages/staff/hggross/IntelligentTimingAnalysis_HTML/node20.html
+#define STDOUT 1
+#define STDERR 2
 
 void main(int argc, char *argv[]) {
   int  wait_val;         // child's return value
   int  pid;              // child's process id
+  int fd = open("/dev/null", "w+");
 
   struct user_regs_struct regs;
 
   switch (pid=fork()) {
   case -1: perror("fork"); break;
   case 0: // child
+    // redirect STDOUT and STDERR to /dev/null
+    if(dup2(fd, STDOUT) != 0) printf("dup stdout failed.");
+    if(dup2(fd, STDERR) != 0) printf("dup stderr failed.");
     ptrace(0,0,0,0);
     // must be called in order to allow the control over the child process
     execvp(argv[1], &argv[1]);
@@ -32,7 +38,7 @@ void main(int argc, char *argv[]) {
     while (wait_val == 1407) {
       if (ptrace(PTRACE_SINGLESTEP,pid,0,0) != 0)        perror("ptrace");
       if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) != 0) perror("ptrace");
-      printf("0x%x\n", regs.rip);
+      printf("%x\n", regs.rip);
       wait(&wait_val);
     }
   }
