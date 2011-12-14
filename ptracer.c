a1e6ff0de80fb618b3b859f30cf96d90b425eb11
Eric Schulte <eric.schulte@gmx.com>
2011-12-13 21:34:30 -0700

    cleaned up, bringing in more tutorials

--- ptracer.c
+++ ptracer.c
@@ -1,19 +1,17 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <syscall.h>
 #include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <sys/user.h>
-// adapted from
-// http://www.comp.glam.ac.uk/pages/staff/hggross/IntelligentTimingAnalysis_HTML/node20.html
 #define STDOUT 1
 #define STDERR 2
 
 void main(int argc, char *argv[]) {
-  int  wait_val;         // child's return value
-  int  pid;              // child's process id
-  int fd = open("/dev/null", "w+");
-
-  struct user_regs_struct regs;
+  int  wait_val;                    // child's return value
+  int fd = open("/dev/null", "w+"); // for redirecting STDIN and STDOUT
+  pid_t  pid;                       // child's process id
+  struct user_regs_struct regs;     // for user_regs_struct
 
   switch (pid=fork()) {
   case -1: perror("fork"); break;
@@ -22,23 +20,17 @@ void main(int argc, char *argv[]) {
     if(dup2(fd, STDOUT) != 0) printf("dup stdout failed.");
     if(dup2(fd, STDERR) != 0) printf("dup stderr failed.");
     ptrace(0,0,0,0);
-    // must be called in order to allow the control over the child process
+    // execute the child process
     execvp(argv[1], &argv[1]);
-    // executes the testprogram and causes the child to stop and send
-    // a signal to the parent, the parent can now switch to PTRACE_SINGLESTEP
     break;
-    // child process ends
   default: // parent
     wait(&wait_val);
-    // parent waits for child to stop (execl)
     if (ptrace(PTRACE_SINGLESTEP,pid,0,0) != 0)          perror("ptrace");
-    // switch to singlestep tracing and release child
     wait(&wait_val);
-    // wait for child to stop at next instruction
     while (wait_val == 1407) {
       if (ptrace(PTRACE_SINGLESTEP,pid,0,0) != 0)        perror("ptrace");
       if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) != 0) perror("ptrace");
-      printf("%x\n", regs.rip);
+      printf("0x%lx is 0x%lx\n", ptrace(PTRACE_PEEKTEXT, pid, regs.rip, NULL));
       wait(&wait_val);
     }
   }
